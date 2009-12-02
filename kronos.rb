91838e1b07c0efb427a2b2bb2236192206c70496
AJ ONeal <coolaj86@gmail.com>
2009-12-01 17:59:24 -0700

    Fixed variable name error

--- kronos.rb
+++ kronos.rb
@@ -4,17 +4,15 @@ require 'mechanize'
 
 class Kronos
 
-  def initialize(server, user, token)
+  def initialize(server)
     @agent = WWW::Mechanize.new
     @server = server
-    @user = user
-    @token = token
     @reply = nil
   end
 
-  def authenticate
-    @agent.get "https://#{server}/wfc/applications/wpk/html/kronos-logonbody.jsp?ESS=true"
-    @reply = @agent.post "https://#{server}/wfc/portal",
+  def authenticate(user, token)
+    @agent.get "https://#{@server}/wfc/applications/wpk/html/kronos-logonbody.jsp?ESS=true"
+    @reply = @agent.post "https://#{@server}/wfc/portal",
       {
         :username => user,
         :password => token,
@@ -26,9 +24,9 @@ class Kronos
   def timestamp(job = nil)
     # TODO strip leading and trailing slashes
     # ... unless... is there actually a reason for them?
-    job = "////#{job}/" unless (job.nil? or job.index('////'))
-    @agent.get "https://#{server}/wfc/applications/wtk/html/ess/timestamp.jsp"
-    @reply = @agent.post "https://#{server}/wfc/applications/wtk/html/ess/timestamp-record.jsp",
+    job = "////#{job}/" #unless (job.nil? or job.index('////'))
+    @agent.get "https://#{@server}/wfc/applications/wtk/html/ess/timestamp.jsp"
+    @reply = @agent.post "https://#{@server}/wfc/applications/wtk/html/ess/timestamp-record.jsp",
       {
         :transfer => job,
       }
@@ -54,7 +52,7 @@ class Kronos
   end
 
   def timecard(preset = nil)
-    navigation = @agent.get "https://#{server}/wfc/applications/mss/managerlaunch.do?ESS=true" #iframe only
+    navigation = @agent.get "https://#{@server}/wfc/applications/mss/managerlaunch.do?ESS=true" #iframe only
     timecard = navigation.links.find {|l| l.text =~ /My Timecard/}.click
     #reply = @agent.get "https://#{server}/wfc/applications/mss/navigation.do?ESS=true" #iframe only
     []
